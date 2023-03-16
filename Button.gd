ff2f870db4d652cb3f07b0ad8320bc37d7bc72e0
igormedeiros <igorjrmedeiros@gmail.com>
2023-03-15 23:18:11 -0300

    update31

---
+++ Button.gd
@@ -0,0 +1,21 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _pressed():
+	var console =  JavaScript.get_interface("console")
+	var window = JavaScript.get_interface("window")
+	console.log("teste")
+	window.login()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
